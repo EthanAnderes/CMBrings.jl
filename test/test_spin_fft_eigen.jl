05b7247cc578e580da500c131d5a4f2381ff56b1
Ethan Anderes <anderes@ucdavis.edu>
2021-03-08 22:40:51 -0800

    simplify test for eigen decomp of spin 2 circ shift

--- test/test_spin_fft_eigen.jl
+++ test/test_spin_fft_eigen.jl
@@ -3,24 +3,24 @@ using SparseArrays
 using PyPlot 
 using FFTW
 
-
 N = 25
-P̃ = spdiagm(-1 => fill(1,N-1))
+R = spdiagm(-1 => fill(1,N-1))
 P = spdiagm(-1 => fill(1,N-1))
-P̃[1,end] = -1
+R[1,end] = -1
 P[1,end] = 1
 
 ω = exp(- im * 2 * π / N)
 ε = exp(- im * π / N)
 
-U = [ ω^( (x-1)*(k-1) ) / √(N) for k ∈ 1:N, x ∈ 1:N ]
-Λ = diagm([ ω^( k-1 )  for k ∈ 1:N  ])
-D = diagm([ ω^( -(x-1)/2 )  for x ∈ 1:N  ])
+U  = [ ω^((x-1)*(k-1)) / √N for k ∈ 1:N, x ∈ 1:N ]
+Λᵣ = diagm([ε*ω^(k-1) for k ∈ 1:N ])
+Λₚ = diagm([  ω^(k-1) for k ∈ 1:N ])
+D  = diagm([  ε^(x-1) for x ∈ 1:N ])
 
-Papprox  =  U' * Λ * U
-P̃approx  =  (1/ε) * D' * U' * Λ * U * D
-P̃approx2 =  (1/ε) * D' * P * D
+P_aprx  =      U' * Λₚ * U
+R_aprx  = D' * U' * Λᵣ * U * D
+R_aprx2 =  ε * D' * P * D
 
-abs.(P .- Papprox) |> sum
-abs.(P̃ .- P̃approx) |> sum
-abs.(P̃ .- P̃approx2) |> sum
+abs.(P .- P_aprx) |> sum
+abs.(R .- R_aprx) |> sum
+abs.(R .- R_aprx2) |> sum
